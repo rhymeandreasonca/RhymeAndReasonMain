cd5a7fbb1d5d9f136d7817b1390b32509be3203b
Brandon <b.lefaivre@gmail.com>
2013-02-17 10:31:54 -0700

    Initial upload excluding videos

---
+++ classhover.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+   $('.tuesmid').hover(function(){
+      $('.tuesmid').css('border','3px solid #57227E');
+   },
+   function(){
+      $('.tuesmid').css('border','3px solid #FF006C');
+   });
+
+   $('.tuesaft').hover(function(){
+      $('.tuesaft').css('border','3px solid #57227E');
+   },
+   function(){
+      $('.tuesaft').css('border','3px solid #2BD6EA');
+   });
+
+   $('.frimorn').hover(function(){
+      $('.frimorn').css('border','3px solid #57227E');
+   },
+   function(){
+      $('.frimorn').css('border','3px solid #70BD17');
+   });
+
+   $('.frimid').hover(function(){
+      $('.frimid').css('border','3px solid #57227E');
+   },
+   function(){
+      $('.frimid').css('border','3px solid #FF006C');
+   });
+
+    $('.satmorn').hover(function(){
+      $('.satmorn').css('border','3px solid #57227E');
+   },
+   function(){
+      $('.satmorn').css('border','3px solid #FF9600');
+   });
+
+   $('.sataft').hover(function(){
+      $('.sataft').css('border','3px solid #57227E');
+   },
+   function(){
+      $('.sataft').css('border','3px solid #FF9600');
+   });
+
+})
